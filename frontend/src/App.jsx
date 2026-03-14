5c4cec222840e877610dd395793254decd40286f
Kim Alfred A. Molina <kimalfred22molina@gmail.com>
2026-03-14 10:46:18 +0800

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='font-bold text-3xl text-center mt-10 text-red-700'>
+      TITE
+    </div>
+  )
+}
+
+export default App
